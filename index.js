0b2ae0dad42a9180c957836540df3d43df7f43d6
Aditya Tawade <adityatwd0@gmail.com>
2022-06-10 18:12:21 +0530

    Download Code Added

--- index.js
+++ index.js
@@ -8,13 +8,14 @@ const s3 = new AWS.S3({
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 });
 
+const folderName = "290"; //Ticket ID Folder Name
+const subFolder = "27007817816"; //Conversation ID Folder Name
 const fileName = "vishalsir.html";
-const folderName = "TicketID";
 const URL = "https://www.google.com";
 
 var params = {
   Bucket: "iconnect-aditya" /* required */,
-  Prefix: "A", // Can be your folder name
+  Prefix: "Hello", // Can be your folder name
 };
 s3.listObjectsV2(params, function (err, data) {
   if (err) console.log(err, err.stack); // an error occurred
@@ -22,6 +23,8 @@ s3.listObjectsV2(params, function (err, data) {
     if (data.KeyCount === 1) uploadFile();
     else {
       console.log("Folder is not present");
+      downloadImage();
+      uploadFile();
     }
   }
 });
@@ -31,7 +34,7 @@ const uploadFile = () => {
     if (err) throw err;
     const params = {
       Bucket: "iconnect-aditya", // pass your bucket name
-      Key: "vishalsir.html", // file will be saved as testBucket/contacts.csv
+      Key: `${folderName}/${subFolder}/vishalsir.html`, // file will be saved as testBucket/contacts.csv
       Body: JSON.stringify(data, null, 2),
     };
     s3.upload(params, function (s3Err, data) {
@@ -39,4 +42,26 @@ const uploadFile = () => {
       console.log(`File uploaded successfully at ${data.Location}`);
     });
   });
+};
+
+const downloadImage = () => {
+  var http = require("http"),
+    Stream = require("stream").Transform,
+    fs = require("fs");
+
+  var url = "https://cutt.ly/kJX8Kg1";
+
+  http
+    .request(url, function (response) {
+      var data = new Stream();
+
+      response.on("data", function (chunk) {
+        data.push(chunk);
+      });
+
+      response.on("end", function () {
+        fs.writeFileSync("image.png", data.read());
+      });
+    })
+    .end();
 };
